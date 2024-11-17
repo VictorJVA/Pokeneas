f80acce6875bf857d12b898c66363c7187ecb0a8
VictorJVA <vjvilladia@eafit.edu.co>
2024-11-17 17:34:36 -0500

    First Commit

---
+++ pokeneas_data.py
@@ -0,0 +1,18 @@
+pokeneas = [
+    {
+        "id": 1,
+        "nombre": "Pokenea1",
+        "altura": 1.8,
+        "habilidad": "Fuerza descomunal",
+        "imagen": "https://storage.googleapis.com/tu-bucket/pokenea1.png",
+        "frase": "La fuerza no proviene de la capacidad física, sino de la voluntad indomable."
+    },
+    {
+        "id": 2,
+        "nombre": "Pokenea2",
+        "altura": 1.6,
+        "habilidad": "Velocidad extrema",
+        "imagen": "https://storage.googleapis.com/tu-bucket/pokenea2.png",
+        "frase": "La vida es como andar en bicicleta: para mantener el equilibrio debes seguir adelante."
+    },
+]
